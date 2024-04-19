4d97ac0a10bef30cd053528c805473e5a5be802b
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2024-04-19 15:16:16 +0100

    Display board

--- isdabizda/Main.hx
+++ isdabizda/Main.hx
@@ -43,6 +43,7 @@ class Main extends hxd.App {
 	var gridWidth:Int;
 	var group:h2d.TileGroup;
 	var tile:h2d.Tile;
+	var board:Array<Array<Null<Int>>>;
 
 	var currentPolyomino:Polyomino;
 
@@ -50,10 +51,17 @@ class Main extends hxd.App {
 		super();
 		gridWidth = 10;
 		gridHeight = 20;
+		board = [for (y in 0...gridHeight) [for (x in 0...gridWidth) null]];
+
+		/* TODO test board code */
+		board[0][3] = 0x1F001F;
+		board[0][4] = 0x1F001F;
+		board[0][5] = 0x1F001F;
+		board[1][4] = 0x1F001F;
+
 	}
 
 	override function init() {
-		tile = h2d.Tile.fromColor(0xFF0000, 1, 1);
 		group = new h2d.TileGroup(tile, s2d);
 		redrawGrid();
 	}
@@ -63,26 +71,42 @@ class Main extends hxd.App {
 		new Main();
 	}
 
+	inline function inverseYIndex(y:Int): Int {
+		return gridHeight - (y % gridHeight) - 1;
+	}
+
 	function redrawGrid() {
 		group.clear();
 		var xRatio = s2d.width / gridWidth;
 		var yRatio = s2d.height / gridHeight;
 		var ratio:Float;
+		var xOffset:Float;
+		var yOffset:Float;
 		if (xRatio * gridHeight < s2d.height) {
 			ratio = xRatio;
+			yOffset = (s2d.height - (ratio * gridHeight)) / 2;
+			xOffset = 0;
 		} else {
 			ratio = yRatio;
+			yOffset = 0;
+			xOffset = (s2d.width - (ratio * gridWidth)) / 2;
 		}
-		tile.setSize(ratio, ratio);
 
-		var x = 0;
-		while (x < gridWidth) {
-			var y = x % 2;
-			while (y < gridHeight) {
-				group.add(x*ratio, y*ratio, tile);
-				y += 2;
+		for (y in 0...gridHeight) {
+			for (x in 0...gridWidth) {
+				var cell = board[inverseYIndex(y)][x];
+				var xPos = x * ratio + xOffset;
+				var yPos = y * ratio + yOffset;
+				if (cell != null) {
+					var tile = h2d.Tile.fromColor(cell, 1, 1);
+					tile.setSize(ratio, ratio);
+					group.add(xPos, yPos, tile);
+				} else {
+					var tile = h2d.Tile.fromColor(0xFFFFFF, 1, 1);
+					tile.setSize(ratio, ratio);
+					group.add(xPos, yPos, tile);
+				}
 			}
-			x += 1;
 		}
 	}
 
